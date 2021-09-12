e1d815ee78be34464e548ad531d5c0c822457212
Castrod043 <64606385+Castrod043@users.noreply.github.com>
2021-09-12 09:53:37 +0200

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,38 @@
+console.log('Function basics: exercise');
+
+
+// Pizza making function 
+
+function makePizza(params) {
+
+    console.log('Maak het deeg');
+    console.log('Rol het deeg uit');
+    console.log('Smeer laag saus op het deeg');
+    console.log('Voeg toppings toe');
+    console.log('Schuif in de oven');
+
+};
+
+
+// sushi making function 
+
+function makeSushi(params) {
+
+    console.log('Kook de rijst');
+    console.log('Leg het noriblad op de sushiroller');
+    console.log('verdeel 2 eetlepels rijst over het noriblad');
+    console.log('leg in het midden de vulling');
+    console.log('Rol de sushi op');
+    console.log('snijd de rol in hapklare stukken;-)');
+    console.log('Eet smakeijk');
+}
+
+// call both functions
+
+makeSushi();
+makeSushi();
+makeSushi();
+
+makePizza();
+makePizza();
+
